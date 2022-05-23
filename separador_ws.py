8ed4d339bf6b7b8597ebf3abc23b25d67a77374c
José Cicero <89671612+joseciceroassisjr@users.noreply.github.com>
2022-05-23 14:10:29 -0300

    Add files via upload

---
+++ separador_ws.py
@@ -0,0 +1,20 @@
+import xlwings as xw
+from pathlib import Path
+
+#Ler o arquivo e criar a pasta de saída
+EXCEL_FILE = "RTIP SIMPLIFICADO.xlsx"
+OUTPUT_DIR = Path(__file__).parent / 'RTIP SIMPLIFICADO'
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+
+#Fazer as cópias das worksheets (planilha de trabalho)
+try: 
+    excel_app = xw.App(visible=False)
+    wb = excel_app.books.open(EXCEL_FILE)
+    for sheet in wb.sheets:
+        sheet.api.Copy()
+        wb_new = xw.books.active
+        wb_new.save(OUTPUT_DIR / f'RTIP {sheet.name} SIMPLIFICADO.xlsx')
+        wb_new.close()
+
+finally:
+    excel_app.quit()
